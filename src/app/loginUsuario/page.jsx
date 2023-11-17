e9a690946d0e99ae85a5a650b99d29b331ae4d69
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-17 11:38:36 -0300

    fondo solucionado

--- src/app/loginUsuario/page.jsx
+++ src/app/loginUsuario/page.jsx
@@ -1,9 +1,13 @@
 import FormularioIniciarSesion from "@/components/FormularioIniciarSesion/FormularioIniciarSesion"
 import FormularioRegistro from "@/components/FormularioRegistro/FormularioRegistro"
 import "../loginUsuario/loginUsuario.css"
+import FondoDinamico from "@/components/FondoDinamico/FondoDinamico"
 const LoginUsuario = ()=>{
     return (
         <div className="loginUsuario">
+            <div className='fondoDinamico'>
+              <FondoDinamico />
+            </div>
             <FormularioIniciarSesion />
             <FormularioRegistro />
         </div>
