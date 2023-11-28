7022c7e74d86aa85464f116e6f6d11fb3f2803e9
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-28 17:48:11 -0300

    detalle agregado

--- src/app/detalle/[id]/page.jsx
+++ src/app/detalle/[id]/page.jsx
@@ -1,3 +1,4 @@
+  "use client"
  import TarjetaHistoria from '@/components/TarjetaHistoria/TarjetaHistoria'
 import '../[id]/detalle.css'
 import TarjetaDetalle from '@/components/TarjetaDetalle/TarjetaDetalle'
@@ -5,7 +6,40 @@ import TarjetaIlustraciones from '@/components/TarjetaIlustraciones/TarjetaIlust
 import Chat from '@/components/Chat/Chat'
 import BotonVolver from '@/components/BotonVolver/BotonVolver'
 import Link from 'next/link'
-const Detalle = ()=>{
+import { addDoc, collection, doc, getDoc, query, where } from 'firebase/firestore'
+import { db } from '@/firebaseConfig'
+import { useEffect, useState } from 'react'
+
+ function Detalle ( {params} ){
+  const [estado, setEstado] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Obtener la referencia al documento
+        const historiaRef = doc(db, 'historias', params.id);
+
+        // Obtener el documento
+        const historiaDoc = await getDoc(historiaRef);
+
+        if (historiaDoc.exists()) {
+          // Actualizar el estado con los datos del documento
+          setEstado(
+            {...historiaDoc.data(), id: historiaDoc.id}
+          );
+        } else {
+          console.log('No se encontró ningún documento con el ID proporcionado.');
+        }
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
+    fetchData();
+  }, [params.id]);
+
+ 
+
+  console.log({ estado });
     return (
         <div className='container__detalle'>
             <div className='botonVolver__detalle'>
@@ -13,9 +47,9 @@ const Detalle = ()=>{
             <BotonVolver />
                 </Link>
             </div>
-            <TarjetaDetalle />
+            <TarjetaDetalle estado={estado} />
                 <TarjetaIlustraciones />
-            <Chat />
+            <Chat params={params} />
         </div>
             
     )
