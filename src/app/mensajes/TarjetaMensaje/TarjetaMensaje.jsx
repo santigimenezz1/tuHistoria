310726b91a26859b625f67ac12f25aeea3e574a5
santiago <santiago.manuel.gimenez97@gmail.com>
2024-03-23 01:01:06 +0100

    detalle corregido

--- src/app/mensajes/TarjetaMensaje/TarjetaMensaje.jsx
+++ src/app/mensajes/TarjetaMensaje/TarjetaMensaje.jsx
@@ -9,11 +9,18 @@ const TarjetaMensaje = ({ comentario }) => {
             <div className='tarjetaMensaje__perfil'>
             <img src='https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699915236/tu%20historia/premium_photo-1687832783320-35671afbf484_ogghp1.jpg' alt="Imagen"></img>
             <div className='tarjetaMensaje__perfil__nombre'>
-              
+                {
+                    comentario &&
+                    <h1>{comentario.user.nombre} {comentario.user.apellido}</h1>
+                }
             </div>
 
             </div>
             <div className='tarjetaMensaje__texto'>
+                {
+                    comentario &&
+                <h1>{comentario.texto}</h1>
+                }
             </div>
         </div>
     );
